b6d1222237179c5984d372c0813884a0454185f6
Jerome Ortega <Zerospace@Jeromes-MacBook-Pro.local>
2018-03-16 06:42:20 -0700

    Display list of surveys to the user

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
-require('./models/User');
-require('./services/passport');
 
+// Require our Mongo Schemas and connect to the DB
+require('./models/User');
+require('./models/Survey');
+require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
@@ -23,9 +25,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Require in all of our routes
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
+// Logic to utilize React Router on the frontend.
 if (process.env.NODE_ENV === 'production') {
     // Express will serve production assets such as main.js / main.css
     app.use(express.static('client/build'));
@@ -36,5 +41,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
+// Declare the port based on our environment and start the server.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/Mailer.js
@@ -0,0 +1,61 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
+
+        this.sgApi = sendgrid(keys.sendGridKey);
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        // This function comes inherited from the helper.Mail object/parent
+        this.addContent(this.body);
+
+        // These functions are helper classes defined below.
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    // Click tracking for sendgrid to assign a unique token to each user responding to the email.
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            // addTo syntax adds each of these to an object.
+            personalize.addTo(recipient);
+        });
+
+        // Defined by mail base class.
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = await this.sgApi.API(request);
+        return response;
+    }
+}
+
+module.exports = Mailer;
